633c9dcf660baf87bb54d50a6f4c5c4b14b2dff0
MatDev435 <matdev435@gmail.com>
2023-07-06 10:13:20 -0300

    Adicionada a rota de remoção de direito de administrador

--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -66,5 +66,52 @@ export async function adminRoutes(app: FastifyInstance) {
         }
     })
 
-    
+    app.delete('/groups/:id/admins/:adminId', async (req, rep) => {
+        const { sub: userId } = req.user;
+
+        const paramsSchema = z.object({
+            id: z.string(),
+            adminId: z.string()
+        });
+
+        const bodySchema = z.object({
+            participantId: z.string()
+        });
+
+        const { id, adminId } = paramsSchema.parse(req.params);
+        const { participantId } = bodySchema.parse(req.body);
+
+        try {
+            const group = await prisma.group.findUnique({
+                where: { id }
+            });
+
+            if(!group) {
+                return rep.status(404).send('Grupo não encontrado');
+            }
+
+            const isAdmin = await isUserGroupAdmin({ userId, groupId: id });
+
+            const admin = await prisma.admin.findFirst({
+                where: {
+                    AND: [
+                        { userId: participantId },
+                        { groupId: group.id }
+                    ]
+                }
+            });
+
+            if(!admin) {
+                return rep.status(404).send('Esse usuário não é um administrador');
+            }
+
+            await prisma.admin.delete({
+                where: { id: adminId }
+            });
+
+            return rep.status(200).send('Removido a permissão de administrador');
+        } catch (error) {
+            return rep.status(500).send('Erro ao tirar a permissão de administrador');
+        }
+    })
 }
