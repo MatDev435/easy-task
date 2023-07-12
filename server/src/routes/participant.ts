18424b4e48bb3f7cda7e3a7dce815f60a881dfc8
MatDev435 <matdev435@gmail.com>
2023-07-12 10:55:51 -0300

    Rota de expulsar um usuário criada

--- server/src/routes/participant.ts
+++ server/src/routes/participant.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { isUserGroupAdmin } from "../utils/isAdmin";
 
 export async function participantRoutes(app: FastifyInstance) {
     app.addHook('preHandler', async (request) => {
@@ -159,9 +160,90 @@ export async function participantRoutes(app: FastifyInstance) {
 
             return rep.status(200).send('Você saiu do grupo');
         } catch (error) {
-            console.log(error);
             return rep.status(500).send('Erro ao sair do grupo');
             
+        }
+    })
+
+    app.delete('/groups/:id/participants/:participantId/kick', async (req, rep) => {
+        const { sub: userId } = req.user;
+
+        const paramsSchema = z.object({
+            id: z.string(),
+            participantId: z.string()
+        });
+
+        const { id, participantId } = paramsSchema.parse(req.params);
+
+        try {
+            const group = await prisma.group.findUnique({
+                where: { id },
+
+                include: { tasks: true }
+            });
+
+            if(!group) {
+                return rep.status(404).send('Grupo não encontrado');
+            }
+
+            const userParticipant = await prisma.participant.findFirst({
+                where: { userId, groupId: group.id }
+            });
+
+            if(!userParticipant) {
+                return rep.status(404).send('Você não faz parte desse grupo');
+            }
+
+            const isAdmin = await isUserGroupAdmin({ userId, groupId: group.id });
+
+            if(!isAdmin) {
+                return rep.status(403).send('Você não tem permissões suficientes');
+            }
+
+            const participant = await prisma.participant.findUnique({
+                where: { id: participantId }
+            });
+
+            if(!participant) {
+                return rep.status(404).send('Esse usuário não faz parte do grupo');
+            }
+
+            if(participant.userId === group.ownerId) {
+                return rep.status(403).send('Você não tem permissões suficientes');
+            }
+
+            const admin = await prisma.admin.findFirst({
+                where: { userId: participant.id, groupId: group.id }
+            });
+
+            if(admin && userParticipant.userId === group.ownerId) {
+                await prisma.admin.delete({
+                    where: { id: admin.id }
+                });
+            }else if(admin && userParticipant.userId !== group.ownerId) {
+                return rep.status(403).send('Você não tem permissões suficientes');
+            }
+
+            if(group.tasks.length > 0) {
+                for(const task of group.tasks) {
+                    await prisma.note.deleteMany({
+                        where: { taskId: task.id, userId: participant.id }
+                    });
+                }
+
+                await prisma.task.deleteMany({
+                    where: { userId: participant.id, groupId: group.id }
+                });
+            }
+
+            await prisma.participant.delete({
+                where: { id: participant.id }
+            });
+
+            return rep.status(200).send('Usuário expulso');
+        } catch (error) {
+            console.log(error);
+            return rep.status(500).send('Erro ao expulsar o usuário');
         }
     })
 }
